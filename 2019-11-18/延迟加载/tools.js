955d46c4ba764416f091930b7356450ca32165fa
pengjincheng1127 <302940522@qq.com>
2019-12-13 22:19:22 +0800

    01

---
+++ 2019-11-18/延迟加载/tools.js
@@ -0,0 +1,21 @@
+class Tools {
+    position(nowEle){
+        let l = 0;
+        let t = 0;
+        let cl = nowEle.clientLeft;
+        let ct = nowEle.clientTop;
+        //只要有当前这个元素就一直循环
+        while(nowEle){
+            l += (nowEle.offsetLeft + nowEle.clientLeft);
+            t += (nowEle.offsetTop + nowEle.clientTop);
+            nowEle = nowEle.offsetParent; //把本次的定位父级变成，下次循环的当前元素。
+        }
+        l = l - cl;
+        t = t - ct;
+
+        return {
+            l,
+            t
+        }
+    }
+}
