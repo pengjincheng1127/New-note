3f80427fe385a60123c0bc2b2e20982de7f6342c
pengjincheng1127 <302940522@qq.com>
2019-12-25 21:57:36 +0800

    2019-12-25

---
+++ 2019-12-25/routers/data/noloading.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/',(req,res)=>{
+    setTimeout(() => {
+        res.json({code:1})
+    }, 3000);
+});
+
+module.exports = router; //导出路由
+
---
+++ 2019-12-25/routers/user/login.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+
+/*
+    router的根不是localhost是 /login/
+*/
+router.post('/',(req,res)=>{
+    setTimeout(()=>{
+        res.json({code:0})
+    },2000)
+})
+module.exports = router;//导出路由
